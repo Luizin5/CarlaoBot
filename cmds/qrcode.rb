0fb4a16b8900d6e2e381cef13981c4129eb542c9
* <luizfelip079@gmail.com>
2022-04-24 14:03:55 -0300

    7

--- cmds/qrcode.rb
+++ cmds/qrcode.rb
@@ -1,8 +1,8 @@
-module Bot
+maodule Bot
   module Qr
     extend Discordrb::Commands::CommandContainer
     command :qr do |event, *txt|
-      event.respond "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=#{txt.join(' ').gsub("","%20")}"
+      event.respond "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=#{txt.join(' ').gsub(" ","%20")}"
     end
   end
 end
